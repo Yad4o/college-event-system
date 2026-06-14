0651f2445d5de4c3541ce944978db5d9278b8cad
Om yadao <omyadao1706@gmail.com>
2026-06-14 09:27:13 +0530

    fix: split sibling SVG elements in Admin.tsx

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -153,7 +153,8 @@ function ApplicationsTab() {
           }
           icon={
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M9 11l3 3L22 4" /><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
+              <path d="M9 11l3 3L22 4" />
+              <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
             </svg>
           }
         />
